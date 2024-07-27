178f29929632cbbbdac1c135c0bc19d66df4b759
Deepak Saini <dee01code@gmail.com>
2024-07-28 04:40:39 +0530

    Migration Updated

--- db/migrate/20240727230345_transaction_expense_association.rb
+++ db/migrate/20240727230345_transaction_expense_association.rb
@@ -1,4 +1,4 @@
-class AddReferenceToTransaction < ActiveRecord::Migration[7.1]
+class TransactionExpenseAssociation < ActiveRecord::Migration[7.1]
   def change
     add_reference :transactions, :expense, null: false, foreign_key: true
   end
